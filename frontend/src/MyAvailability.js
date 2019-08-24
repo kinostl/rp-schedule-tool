3a832b2cdd6eaff65cb284a007a16040e20575fe
Minty <kinostl@gmail.com>
2019-08-24 18:43:58 -0400

    Proper combines

--- frontend/src/MyAvailability.js
+++ frontend/src/MyAvailability.js
@@ -10,6 +10,71 @@ export default class MyAvailability extends React.Component {
 		this.calRef = React.createRef()
 	}
 
+	getConflicts = (newEvent, event) => {
+		let startsInEvent = ((newEvent.start >= event.start) && (newEvent.start <= event.end))
+		let endsInEvent = ((newEvent.end >= event.start) && (newEvent.end <= event.end))
+		let eventInNewEvent = ((newEvent.start <= event.start) && (newEvent.end >= event.end))
+		return [startsInEvent, endsInEvent, eventInNewEvent]
+	}
+
+	addEvent = (newEvent) => {
+		let calApi = this.calRef.current.getApi()
+		let events = calApi.getEvents()
+		if(!newEvent){
+			newEvent = this.state.selected_range
+		}
+		let makeNewEvent = true
+		for (let event of events) {
+			let [startsInEvent, endsInEvent, eventInNewEvent] = this.getConflicts(newEvent, event)
+			if (event.allDay === false) {
+				if (startsInEvent) {
+					makeNewEvent = false
+					event.setEnd(newEvent.end)
+				}
+				if (endsInEvent) {
+					makeNewEvent = false
+					event.setStart(newEvent.start)
+				}
+				if (eventInNewEvent) {
+					event.remove()
+				}
+			}
+		}
+		if (makeNewEvent) {
+			calApi.addEvent(this.state.selected_range)
+		}
+	}
+	
+	removeEvent = () => {
+		let calApi = this.calRef.current.getApi()
+		let events = calApi.getEvents()
+		let newEvent = this.state.selected_range
+		for (let event of events) {
+			console.log({
+				"event":event,
+				"newEvent":newEvent
+			})
+			let [startsInEvent, endsInEvent, eventInNewEvent] = this.getConflicts(newEvent, event)
+			if (startsInEvent) {
+				event.setEnd(newEvent.start)
+			}
+			if (endsInEvent) {
+				event.setStart(newEvent.end)
+			}
+			if (eventInNewEvent) {
+				event.remove()
+			}
+		}
+	}
+
+	handleResize = (info) => {
+		this.addEvent(info.event)
+	}
+
+	handleDrop = (info) => {
+		this.addEvent(info.event)
+	}
+
 	render() {
 		return (
 			<div>
@@ -17,7 +82,7 @@ export default class MyAvailability extends React.Component {
 				<div style={{ "display": "flex", "justifyContent": "space-around" }}>
 					<div style={{ "width": "25%" }}>
 						<h2>My Servers</h2>
-						<ul style={{"listStyle":"none"}}>
+						<ul style={{ "listStyle": "none" }}>
 							<li><input type="checkbox" /> Select All</li>
 							<li><input type="checkbox" /> Server 1</li>
 							<li><input type="checkbox" /> Server 2</li>
@@ -34,47 +99,11 @@ export default class MyAvailability extends React.Component {
 							: (<div className="form-group">&nbsp;</div>)
 						}
 						<div className="form-group" style={{ "display": "flex", "justifyContent": "space-around" }}>
-							<button className="btn btn-success" onClick={() => {
-								let calApi = this.calRef.current.getApi()
-								let events = calApi.getEvents()
-								let newEvent = this.state.selected_range
-								let makeNewEvent = true
-								for(let event of events){
-									let startsInEvent = ((newEvent.start >= event.start) && (newEvent.start <= event.end))
-									let endsInEvent = ((newEvent.end >= event.start) && (newEvent.end <= event.end))
-									let eventInNewEvent = ((newEvent.start <= event.start) && (newEvent.end >= event.end))
-									console.table({
-										"startsInEvent":startsInEvent,
-										"endsInEvents":endsInEvent,
-										"eventInNewEvent":eventInNewEvent
-									})
-									if(event.allDay === false){
-										if(startsInEvent){
-											makeNewEvent=false
-											event.setEnd(newEvent.end)
-										}
-										if(endsInEvent){
-											makeNewEvent=false
-											event.setStart(newEvent.start)
-										}
-										if(eventInNewEvent){
-											event.remove()
-										}
-									}
-								}
-								if(makeNewEvent){
-									calApi.addEvent(this.state.selected_range)
-								}
-							}}>
-								Available
-							</button>
-							<button className="btn btn-danger">
-								Unavailable
-							</button>
+							<button className="btn btn-success" onClick={this.addEvent}>Available</button>
+							<button className="btn btn-danger" onClick={this.removeEvent}>Unavailable</button>
 							<button className="btn btn-primary" onClick={() => {
 								let calApi = this.calRef.current.getApi()
 								let events = calApi.getEvents()
-								console.log(events)
 							}}>
 								Save
 							</button>
@@ -89,6 +118,8 @@ export default class MyAvailability extends React.Component {
 									"selected_range": selectInfo
 								})
 							}}
+							eventResize={this.handleResize}
+							eventDrop={this.handleDrop}
 							events={[
 								{ date: '2019-08-23' },
 								{ date: '2019-08-25' }
